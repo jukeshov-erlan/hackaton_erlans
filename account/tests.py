99d2f9b27c82eeb08f93d3b6c804f3a3cfc2512b
erlan.jukeshovich <jukeshov.erlan@gmail.com>
2024-02-17 20:22:46 +0600

    adding testing in product and account

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,106 @@
-from django.test import TestCase
+# from rest_framework.test import APITestCase
+# from django.urls import reverse
+# from account.models import User
 
-# Create your tests here.
+# class AutoApiTestCase(APITestCase):
+#     def test_get(self):
+#         user_1 = User.objects.create(email='user1@gmail.com', name='user1')
+#         user_2 = User.objects.create(email='user2@gmail.com', name='user2')
+    
+#         url = 'api/v1/account/register/'
+#         # print(url)
+#         print('-------------')
+#         response = self.client.get(url)
+#         # print(response.content)
+
+# from django.test import TestCase
+
+from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
+from django.contrib.auth import get_user_model
+from .views import *
+from django.contrib.auth.hashers import make_password
+
+
+User = get_user_model()
+
+class AuthTest(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create_user(
+            email='user@gmail.com',
+            password='12345678',
+            is_active=True,
+            activation_code='1234'
+        )
+    
+    def test_registration(self):
+        data = {
+            'email': 'new_test_user@gmail.com',
+            'password': '12345678',
+            'password_confirm': '12345678',
+            'name': 'test'
+        }
+
+        request = self.factory.post('api/v1/account/register/', data, format='json')
+        print(request)
+        print('===========')
+        view = RegistrationView.as_view()
+        response = view(request)
+        print(response)
+        print('===========')
+        assert User.objects.filter(email=data['email']).exists()
+        # assert response.status_code == 201
+
+
+    def test_login(self):
+        data = {
+            'email': 'user@gmail.com',
+            'password': '12345678'
+        }
+        request = self.factory.post('login/', data, format='json')
+        view = LoginView.as_view()
+        response = view(request)
+        assert 'token' in response.data
+
+    def test_logout(self):
+        data = {
+            'email': 'user@gmail.com',
+            'password': '12345678'
+        }
+        request = self.factory.post('logout/', data, format='json')
+        view = LogoutView.as_view()
+        response = view(request)
+        assert User.objects.filter(email=data['email']).delete()
+
+    def test_change_password(self):
+        data = {
+            'old_password': '12345678',
+            'new_password': '1234567a',
+            'new_password_confirm': '1234567a'
+        }
+        request = self.factory.post('change_password', data, format='json')
+        force_authenticate(request, user=self.user)
+        view = ChangePasswordView.as_view()
+        response = view(request)
+        assert response.status_code == 200
+
+
+    def test_forgot_password(self):
+        request = self.factory.post('forgot-password/', data = {'email': 'user@gmail.com'}, format='json')
+        view = ForgotPasswordView.as_view()
+        response = view(request)
+        assert response.status_code == 200
+
+    def test_forgot_pass_complete(self):
+        data = {
+                'email': 'user@gmail.com', 
+                'code': '1234', 
+                'password': '1234test', 
+                'password_confirm': '1234test'
+                }
+        request = self.factory.post('forgot-password-complete/', data, format='json')
+        view = ForgotPasswordCompleteView.as_view()
+        response = view(request)
+        print(response.data)
+        assert response.status_code == 200
+
--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Category, Auto
 
-# Create your tests here.
+User = get_user_model()
+
+class ModelsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='test@example.com', password='12345')
+        self.category = Category.objects.create(name='Test Category')
+        self.auto = Auto.objects.create(
+            category=self.category,
+            mark='Test Mark',
+            model='Test Model',
+            type_of_body='SEDAN',
+            transmission='AUTOMAT',
+            wheel='LEFT',
+            color='BLACK',
+            location='Test Location',
+            price=10000,
+            ranting='FROM 6 TO 24 HOURS',
+            body='Test Content',
+            number='1234567890',
+            user=self.user
+        )
+
+    def test_category_creation(self):
+        category = Category.objects.get(name='Test Category')
+        self.assertEqual(category.name, 'Test Category')
+
+    def test_auto_creation(self):
+        auto = Auto.objects.get(mark='Test Mark')
+        self.assertEqual(auto.model, 'Test Model')
+        self.assertEqual(auto.type_of_body, 'SEDAN')
+        self.assertEqual(auto.transmission, 'AUTOMAT')
+        self.assertEqual(auto.wheel, 'LEFT')
+        self.assertEqual(auto.color, 'BLACK')
+        self.assertEqual(auto.location, 'Test Location')
+        self.assertEqual(auto.price, 10000)
+        self.assertEqual(auto.ranting, 'FROM 6 TO 24 HOURS')
+        self.assertEqual(auto.body, 'Test Content')
+        self.assertEqual(auto.number, '1234567890')
+        self.assertEqual(auto.user, self.user)
