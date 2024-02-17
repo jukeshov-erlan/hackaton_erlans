cc79f6092b923c2b85b0e59f9c9dc86d6a467000
erlan.jukeshovich <jukeshov.erlan@gmail.com>
2024-02-17 20:47:19 +0600

    adding swagger and redoc

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,31 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Hackaton 2",
+      default_version='v1',
+      description="this is for hackaton 2 by Erlan and Erlan",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="ersik.j10@gmail.com"),
+      license=openapi.License(name="Apache License 2.0"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/account/', include('account.urls')),
     path('api/v1/product/', include('product.urls')),
     path('api/v1/reviews/', include('review.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 
 ]
